9f627852226b98ce9b43fa183005b1aa5a535362
Anthony BARA <banthony@e1r8p10.42.fr>
2018-04-11 19:33:04 +0200

    Recherche de police exploitable (monospace)

--- src/map_creator.c
+++ src/map_creator.c
@@ -6,7 +6,7 @@
 /*   By: banthony <banthony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 15:58:11 by banthony          #+#    #+#             */
-/*   Updated: 2018/04/11 18:21:32 by banthony         ###   ########.fr       */
+/*   Updated: 2018/04/11 19:29:20 by banthony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ int					eventk_map_creator(int keyhook, void *wolf)
 		return (0);
 	if (keyhook == MLX_KEY_ESCAPE)
 		w->current_page = MAIN_MENU;
+	else if (keyhook == MLX_KEY_DOWN)
+		w->font_cursor.y += 32;
+	else if (keyhook == MLX_KEY_RIGHT)
+		w->font_cursor.x += 32;
+	else if (keyhook == MLX_KEY_UP)
+		w->font_cursor.y -= 32;
+	else if (keyhook == MLX_KEY_LEFT)
+		w->font_cursor.x -= 32;
 	return (0);
 }
 
@@ -65,6 +73,26 @@ static void display_font(t_img *dest, t_img *text, t_wolf *w)
 	}
 }
 
+static void target(int x, int y, int square, t_img *img)
+{
+	t_coord i;
+
+	i.y = y;
+	while (i.y < y + square)
+	{
+		i.x = x;
+		while (i.x < x + square)
+		{
+			if (i.x == x || i.y == y || i.x == x + square || i.y == y + square)
+				put_pixel_img(i, 0x00ff00, img);
+			if (i.x == x + square-1 || i.y == y + square -1)
+				put_pixel_img(i, 0x00ff00, img);
+			i.x++;
+		}
+		i.y++;
+	}
+}
+
 void	draw_map_creator(void *wolf)
 {
 	t_wolf *w;
@@ -76,6 +104,12 @@ void	draw_map_creator(void *wolf)
 	pt.y = w->img_size[MAP_CREATOR].y / 2;
 	put_pixel_img(pt, 0x0000ff, &w->img[MAP_CREATOR]);
 	display_font(&w->img[MAP_CREATOR], &w->texture[T_POLICE], w);
+	if (!w->font_cursor.x && !w->font_cursor.y)
+	{
+		w->font_cursor.x = 55;
+		w->font_cursor.y = 148;
+	}
+	target(w->font_cursor.x, w->font_cursor.y, 32, &w->img[MAP_CREATOR]);
 	(void)wolf;
 }
 
